adc12634c792080a529c314da033aaf9266fd012
Jason Mason <jasonjaymason@gmail.com>
2025-05-11 18:08:48 -0600

    Completed payload boiler.

--- apps/web/src/lib/components/site/blocks/sections/Hero.tsx
+++ apps/web/src/lib/components/site/blocks/sections/Hero.tsx
@@ -5,7 +5,9 @@ import { HeroBlock } from "@/payload-types";
 export const HeroSection: React.FC<HeroBlock> = (p) => {
   return (
     <Section section={p.section} className="pt-3">
-      {p.headline}
+      <button className="bg-foreground text-background">
+        {p.primaryCta.innerText}
+      </button>
     </Section>
   );
 };
