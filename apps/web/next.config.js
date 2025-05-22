9bfe7881f74875334ad0ee600ac8a5c1c6e0e393
Jason Mason <jasonjaymason@gmail.com>
2025-05-22 15:05:42 -0600

    Fix posthog?

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,19 +4,20 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/ph/static/:path*",
+        source: "/ingest/static/:path*",
         destination: "https://us-assets.i.posthog.com/static/:path*",
       },
       {
-        source: "/ph/:path*",
+        source: "/ingest/:path*",
         destination: "https://us.i.posthog.com/:path*",
       },
       {
-        source: "/ph/decide",
+        source: "/ingest/decide",
         destination: "https://us.i.posthog.com/decide",
       },
     ];
   },
+  // This is required to support PostHog trailing slash API requests
   skipTrailingSlashRedirect: true,
 };
 
--- apps/web/src/lib/posthog/provider.tsx
+++ apps/web/src/lib/posthog/provider.tsx
@@ -12,10 +12,7 @@ import { env } from "@/config/env";
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host:
-        process.env.NODE_ENV === "production"
-          ? env.NEXT_PUBLIC_POSTHOG_HOST
-          : "https://us.i.posthog.com",
+      api_host: "/ingest",
       ui_host: "https://us.posthog.com",
       person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
