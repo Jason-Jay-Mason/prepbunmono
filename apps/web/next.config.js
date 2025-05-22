e987eac4f414d874f8874346f8b50abafe63a3ba
Jason Mason <jasonjaymason@gmail.com>
2025-05-22 14:26:27 -0600

    Add posthog redirects

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,22 @@
 import { withPayload } from "@payloadcms/next/withPayload";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/ph/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ph/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+      {
+        source: "/ph/decide",
+        destination: "https://us.i.posthog.com/decide",
+      },
+    ];
+  },
+};
 
 export default withPayload(nextConfig);
