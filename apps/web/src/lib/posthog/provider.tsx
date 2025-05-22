7fd0668c17c54807242306dd3a7c5f7c63ccd926
Jason Mason <jasonjaymason@gmail.com>
2025-05-22 15:02:03 -0600

    Fix posthog?

--- apps/web/src/lib/posthog/provider.tsx
+++ apps/web/src/lib/posthog/provider.tsx
@@ -16,6 +16,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
         process.env.NODE_ENV === "production"
           ? env.NEXT_PUBLIC_POSTHOG_HOST
           : "https://us.i.posthog.com",
+      ui_host: "https://us.posthog.com",
       person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
     });
